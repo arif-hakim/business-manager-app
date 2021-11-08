d8d773e3e7ce9cca421c9f4d6f8beda5e1ebb9de
artouro <sir.louis.trenet@gmail.com>
2021-11-09 01:21:18 +0700

    Change Log # Fixes - Business Category Handler

--- handlers/businessCategory.js
+++ handlers/businessCategory.js
@@ -6,17 +6,20 @@ const { BusinessCategoryType } = require('../types')
 const businessCategoryUsecase = require('../usecases/businessCategory')
 
 module.exports = () => ({
-  businessCategories: {
-    type: new GraphQLList(BusinessCategoryType),
-    description: 'List of All Business Categories',
-    resolve: async () => await businessCategoryUsecase.getAll(),
-  },
-  businessCategory: {
-    type: BusinessCategoryType,
-    description: 'Get a single Business Category',
-    args: {
-      id: { type: GraphQLInt }
+  queries: {
+    businessCategories: {
+      type: new GraphQLList(BusinessCategoryType),
+      description: 'List of All Business Categories',
+      resolve: async () => await businessCategoryUsecase.getAll(),
     },
-    resolve: async (_, { id }) => await businessCategoryUsecase.getById(id),
+    businessCategory: {
+      type: BusinessCategoryType,
+      description: 'Get a single Business Category',
+      args: {
+        id: { type: GraphQLInt }
+      },
+      resolve: async (_, { id }) => await businessCategoryUsecase.getById(id),
+    },
   },
+  mutations: {}
 })
